411ded2fc929dd3c43403290bd7fcf8ad8267982
phong.ngo <phong.trieungo@hotmail.com>
2019-11-06 08:43:46 +0700

    #0000: adding fireabase key to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAWzYQva5icv4tULwRF6sB4qon8iWOOSRk',
+        authDomain: 'management-24d58.firebaseapp.com',
+        databaseURL: 'https://management-24d58.firebaseio.com',
+        projectId: 'management-24d58',
+        storageBucket: 'management-24d58.appspot.com',
+        messagingSenderId: '344861893582',
+        appId: '1:344861893582:web:9b49f22b94a0896548c19f',
+        measurementId: 'G-XCFV3FJSPE'
+    }
 };
